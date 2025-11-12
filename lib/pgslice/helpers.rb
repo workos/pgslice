23f304d89f47c95dd7c00e7569a19cdfb64a0fa8
Naveen Lekkalapudi <naveen@workos.com>
2025-11-12 16:52:16 -0500

    support prefixes

--- lib/pgslice/helpers.rb
+++ lib/pgslice/helpers.rb
@@ -184,7 +184,9 @@ module PgSlice
     # ULID helper methods
     def ulid?(value)
       return false unless value.is_a?(String)
-      value.match?(/\A[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}\z/)
+      # Match pure ULIDs or ULIDs with prefixes
+      value.match?(/\A[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}\z/) ||
+        value.match?(/.*[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}\z/)
     end
 
     def numeric_id?(value)
