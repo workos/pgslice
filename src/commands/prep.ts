f8561e91d067ce15d3165ab193deeacf87c09225
Michael Hadley <m@mthadley.com>
2026-01-21 08:56:01 -0800

    Remove debug logging

--- src/commands/prep.ts
+++ src/commands/prep.ts
@@ -40,16 +40,11 @@ export class PrepCommand extends BaseCommand {
   });
 
   override async perform(tx: DatabaseTransactionConnection): Promise<void> {
-    try {
-      await this.context.pgslice.prep(tx, {
-        table: this.table,
-        column: this.column,
-        period: this.period as Period | undefined,
-        partition: this.partition,
-      });
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    await this.context.pgslice.prep(tx, {
+      table: this.table,
+      column: this.column,
+      period: this.period as Period | undefined,
+      partition: this.partition,
+    });
   }
 }
